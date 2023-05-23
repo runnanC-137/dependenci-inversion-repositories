055d1b39c0c25d84cb5fed68c36074be0224a6ad
Ruan Vieira <79387455+runnanC-137@users.noreply.github.com>
2023-05-23 19:34:31 -0300

    Update prisma-user-repository.ts

--- prisma-user-repository.ts
+++ prisma-user-repository.ts
@@ -1,6 +1,8 @@
-
+// contract
 import { UserRepository } from '../user-repository'
+// entities
 import { User } from 'src/entities/user-entities' 
+// service
 import { PrismaService } from 'src/database/prisma.service'
 import { Injectable } from '@nestjs/common'
 
