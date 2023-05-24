1da8b1c05929b023021d141b4ad80ea9ffbf9379
Ruan Vieira <79387455+runnanC-137@users.noreply.github.com>
2023-05-24 14:52:31 +0000

    add

---
+++ contact/user-repository.ts
@@ -0,0 +1,10 @@
+import { User } from '../entities/user';
+
+export abstract class UserRepository {
+  abstract create: (user: User) => Promise<User>;           // C
+  abstract read: (userId: string) => Promise<User>;         // R - read
+  abstract readAll: () => Promise<User[]>;                  // R - read all
+  abstract readByEmail: (email: string) => Promise<User>;   // R - read by public key
+  abstract update: (user: User) => Promise<User>;           // U
+  abstract delete: (userId: string) => Promise<void>;       // U
+}
