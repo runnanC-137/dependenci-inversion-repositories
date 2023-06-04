34363bef29aa61fcd9c9e5299f3c9b345f777d31
Ruan Vieira <79387455+runnanC-137@users.noreply.github.com>
2023-06-04 00:33:03 +0000

    feat

--- ts/contracts/user.repository.ts
+++ ts/contracts/user.repository.ts
@@ -1,10 +1,10 @@
 import { User } from '../../entities-models/entities-ts/user.entity'
 
 export abstract class UserRepository {
-  abstract create: (user: User) => Promise<User>;           // C
-  abstract read: (userId: string) => Promise<User>;         // R - read
-  abstract readAll: () => Promise<User[]>;                  // R - read all
-  abstract readByEmail: (email: string) => Promise<User>;   // R - read by public key
-  abstract update: (user: User) => Promise<User>;           // U
-  abstract delete: (userId: string) => Promise<void>;       // U
+  abstract create: (user: User) => Promise<User>;                  // C
+  abstract read: (userId: string) => Promise<User | null>;         // R - read
+  abstract readAll: () => Promise<User[]>;                         // R - read all
+  abstract readByEmail: (email: string) => Promise<User | null>;   // R - read by public key
+  abstract update: (user: User) => Promise<User>;                  // U
+  abstract delete: (userId: string) => Promise<void>;              // U
 }
